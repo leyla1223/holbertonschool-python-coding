896f9d850d9930e469b19cf0b9813ef70a7e33d3
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 07:31:53 +0000

    Area of square

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a Square class with validated size attribute."""
+
+
+class Square:
+    """Defines a square with a private instance attribute: size."""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
