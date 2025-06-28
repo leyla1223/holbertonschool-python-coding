7cd7eed98f43c4badfd1c49eefee30d5363949e8
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 07:46:55 +0000

    Do not like set and get

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a Square class with property getters and setters."""
+
+
+class Square:
+    """Defines a square with size validation and area computation."""
+    def __init__(self, size=0):
+        self.size = size
+     
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
