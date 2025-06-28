01b6b4692d485131e2dcd67f087b329aaf4920a5
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 07:13:59 +0000

    too much kapriz

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a Square class with a private size attribute."""
 class Square:
+    """Defines a square by a private instance attribute size."""
     def __init__(self, size):
         self.__size = size
