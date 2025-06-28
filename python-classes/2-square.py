c8152d61d51b94c7ddbdbf35469db242bbe04dd4
leyla1223 <leylamuradli2016@gmail.com>
2025-06-28 07:33:19 +0000

    pep probs

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,5 +11,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+
         """Return the current square area."""
         return self.__size ** 2
